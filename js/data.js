69c7f240933a8d759dc62f6157baf1fa8355943a
SimoNapo02 <simonematteonapolitano@gmail.com>
2022-05-17 18:06:08 +0200

    js

--- js/data.js
+++ js/data.js
@@ -118,3 +118,14 @@ const icone = [
 // Milestone 1
 // Partendo dalla struttura dati fornita, visualizzare in pagina un box per ogni icona, in cui è presente il nome dell'icona e l'icona stessa.
 
+const container = document.querySelector (".container")
+for (let i = 0; i < icone.length; i++) {
+    const element = icone[i];
+    const boxHtml = `
+		<div class="box-icon">
+			<i class="${element.family} ${element.prefix + element.name}" style = "color: ${element.color}";></i>
+			<span>${element.name}</span>
+		</div>
+	`
+    container.innerHTML += boxHtml;
+}
